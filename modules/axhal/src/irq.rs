d68b7d77ce9414223d87a6e536c51fb7f1e5429b
hky1999 <keyang.hu@qq.com>
2024-11-20 22:32:30 +0800

    [feat] support irq handling routine from arceos-umhv (#33)

--- modules/axhal/src/irq.rs
+++ modules/axhal/src/irq.rs
@@ -38,6 +38,11 @@ pub(crate) fn register_handler_common(irq_num: usize, handler: IrqHandler) -> bo
     false
 }
 
+/// Core IRQ handling routine, registered at `axhal::trap::IRQ`,
+/// which dispatches IRQs to registered handlers.
+///
+/// Note: this function is denoted as public here because it'll be called by the
+/// hypervisor for hypervisor reserved IRQ handling.
 #[register_trap_handler(IRQ)]
 pub fn handler_irq(irq_num: usize) -> bool {
     let guard = kernel_guard::NoPreempt::new();
